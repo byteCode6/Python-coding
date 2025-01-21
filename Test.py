78ee564311527f0a6713a1a431bb47ff1c34bdd4
byteCode6 <119801858+byteCode6@users.noreply.github.com>
2025-01-21 18:01:18 +0600

    A
    This is my Factrial code

---
+++ Test.py
@@ -0,0 +1,15 @@
+def factrial (f):
+    if f < 0 :
+        return "Factrial is not Defined for negtive number."
+    elif f == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, f+1):
+            result *= i
+        return result
+
+a = factrial(5)
+print(a,'!')   
+    
+    
